7cb4b7ddbf50b50ad0c68bf55527d55b89646c68
asc <0861956@hr.nl>
2014-05-14 20:01:49 +0200

    Some changes

---
+++ site/web/config.php
@@ -0,0 +1,14 @@
+<?php
+
+define('DEBUG', true);
+
+if(DEBUG == true)
+{
+    ini_set('display_errors', 'On');
+    error_reporting(E_ALL);
+}
+else
+{
+    ini_set('display_errors', 'Off');
+    error_reporting(0);
+}
