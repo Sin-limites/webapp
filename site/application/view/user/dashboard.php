64e811f8155a48bd33a4dd2ec37ee432835d38cd
Axel Schelfhout [VM] <axel.schelfhout@mindbench.nl>
2014-05-20 11:19:57 +0200

    Fixed missing dashboard info

--- site/application/view/user/dashboard.php
+++ site/application/view/user/dashboard.php
@@ -48,18 +48,42 @@
   <div class="intro">
     <div id="banner">
       <img src="/res/images/banner_01.png" />
->>>>>>> axel
     </div>
-  
-    <!-- footer -->
-    <footer>
-      <div class="footer">
-        Copyright &copy; Sin Limites, 2014
-      </div><!-- /footer -->
-    </footer>
-  </div><!-- /container -->
-  
-  </body>
-  <!-- key=AIzaSyDGwUpTVd6XKuYSpd2dJeEBfC4KDCYxNIs& -->
+  </div><!-- /intro -->
+
+  <!-- content -->
+  <div class="holder_content">
+    <section class="group1_about">
+      <h3>Over ons..</h3>
+      <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec molestie. Sed aliquam sem ut arcu. Phasellus sollicitudin. 
+      Vestibulum condimentum  facilisis nulla. In hac habitasse platea dictumst. Nulla nonummy. Cras quis libero.</p>
+      <a href="#">Meer..</a>
+    </section>
+
+    <section class="group2_service">
+      <h3>Service</h3>
+      <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec molestie. Sed aliquam sem ut arcu. Phasellus sollicitudin. 
+      Vestibulum condimentum  facilisis nulla. In hac habitasse platea dictumst. Nulla nonummy. Cras quis libero.</p>
+      <a href="#">Meer..</a>
+    </section>
+
+    <section class="group3_contact">
+      <h3>Contact</h3>
+      <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec molestie. Sed aliquam sem ut arcu. Phasellus sollicitudin. 
+      Vestibulum condimentum  facilisis nulla. In hac habitasse platea dictumst. Nulla nonummy. Cras quis libero.</p>
+      <a href="#">Meer..</a>
+    </section>
+  </div>
+
+  <!-- footer -->
+  <footer>
+    <div class="footer">
+      Copyright &copy; Sin Limites, 2014
+    </div><!-- /footer -->
+  </footer>
+</div><!-- /container -->
+
+</body>
+<!-- key=AIzaSyDGwUpTVd6XKuYSpd2dJeEBfC4KDCYxNIs& -->
 
 </html>
