da473b10cd8700d4b1428cf99a6fab24dce9bd58
Axel Schelfhout [VM] <axel.schelfhout@mindbench.nl>
2014-05-20 10:30:58 +0200

    Changed something

--- site/application/view/user/dashboard.php
+++ site/application/view/user/dashboard.php
@@ -23,69 +23,69 @@ include_once('../app/includes.php');
   
 </head>
 
-<body>
-
-
-<!-- container -->
-<div class="container">
-
-  <!-- header -->
-  <div class="header">
-    <!-- logo -->
-    <div class="logo">
-      <a href=""><img src="/res/images/logo.png"></a>
-    </div><!-- /logo -->
-
-    <!-- nav -->
-    <div class="nav"><span>
-      <ul>
-        <li class="current"><a href="#">home</a></li>
-        <li><a href="#data.php">data</a></li>
-        <li><a href="#map.php">map</a></li>
-      </ul></span>
-    </div><!-- /nav -->
-  </div><!-- /header -->
-
-  <!-- intro -->
-  <div class="intro">
-    <div id="banner">
-      <img src="/res/images/banner_01.png" />
+  <body>
+  
+  
+  <!-- container -->
+  <div class="container">
+  
+    <!-- header -->
+    <div class="header">
+      <!-- logo -->
+      <div class="logo">
+        <a href=""><img src="/res/images/logo.png"></a>
+      </div><!-- /logo -->
+  
+      <!-- nav -->
+      <div class="nav"><span>
+        <ul>
+          <li class="current"><a href="#">home</a></li>
+          <li><a href="#data.php">data</a></li>
+          <li><a href="#map.php">map</a></li>
+        </ul></span>
+      </div><!-- /nav -->
+    </div><!-- /header -->
+  
+    <!-- intro -->
+    <div class="intro">
+      <div id="banner">
+        <img src="/res/images/banner_01.png" />
+      </div>
+    </div><!-- /intro -->
+  
+    <!-- content -->
+    <div class="holder_content">
+      <section class="group1_about">
+        <h3>Over ons..</h3>
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec molestie. Sed aliquam sem ut arcu. Phasellus sollicitudin. 
+        Vestibulum condimentum  facilisis nulla. In hac habitasse platea dictumst. Nulla nonummy. Cras quis libero.</p>
+        <a href="#">Meer..</a>
+      </section>
+  
+      <section class="group2_service">
+        <h3>Service</h3>
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec molestie. Sed aliquam sem ut arcu. Phasellus sollicitudin. 
+        Vestibulum condimentum  facilisis nulla. In hac habitasse platea dictumst. Nulla nonummy. Cras quis libero.</p>
+        <a href="#">Meer..</a>
+      </section>
+  
+      <section class="group3_contact">
+        <h3>Contact</h3>
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec molestie. Sed aliquam sem ut arcu. Phasellus sollicitudin. 
+        Vestibulum condimentum  facilisis nulla. In hac habitasse platea dictumst. Nulla nonummy. Cras quis libero.</p>
+        <a href="#">Meer..</a>
+      </section>
     </div>
-  </div><!-- /intro -->
-
-  <!-- content -->
-  <div class="holder_content">
-    <section class="group1_about">
-      <h3>Over ons..</h3>
-      <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec molestie. Sed aliquam sem ut arcu. Phasellus sollicitudin. 
-      Vestibulum condimentum  facilisis nulla. In hac habitasse platea dictumst. Nulla nonummy. Cras quis libero.</p>
-      <a href="#">Meer..</a>
-    </section>
-
-    <section class="group2_service">
-      <h3>Service</h3>
-      <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec molestie. Sed aliquam sem ut arcu. Phasellus sollicitudin. 
-      Vestibulum condimentum  facilisis nulla. In hac habitasse platea dictumst. Nulla nonummy. Cras quis libero.</p>
-      <a href="#">Meer..</a>
-    </section>
-
-    <section class="group3_contact">
-      <h3>Contact</h3>
-      <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec molestie. Sed aliquam sem ut arcu. Phasellus sollicitudin. 
-      Vestibulum condimentum  facilisis nulla. In hac habitasse platea dictumst. Nulla nonummy. Cras quis libero.</p>
-      <a href="#">Meer..</a>
-    </section>
-  </div>
-
-  <!-- footer -->
-  <footer>
-    <div class="footer">
-      Copyright &copy; Sin Limites, 2014
-    </div><!-- /footer -->
-  </footer>
-</div><!-- /container -->
-
-</body>
-<!-- key=AIzaSyDGwUpTVd6XKuYSpd2dJeEBfC4KDCYxNIs& -->
+  
+    <!-- footer -->
+    <footer>
+      <div class="footer">
+        Copyright &copy; Sin Limites, 2014
+      </div><!-- /footer -->
+    </footer>
+  </div><!-- /container -->
+  
+  </body>
+  <!-- key=AIzaSyDGwUpTVd6XKuYSpd2dJeEBfC4KDCYxNIs& -->
 
 </html>
